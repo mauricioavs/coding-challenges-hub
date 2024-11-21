01b391b3a29a7f833a86b2bb69ddd102c663c518
mauricioavs <mauriciovelsa99@gmail.com>
2024-11-21 09:22:58 -0600

    fixed a solution.sql from sql project planning problem

--- HackerRank/SQL/Medium/SQL_Project_Planning/solution.sql
+++ HackerRank/SQL/Medium/SQL_Project_Planning/solution.sql
@@ -1,8 +1,20 @@
-SELECT
-S.Name
-FROM STUDENTS S
-LEFT JOIN FRIENDS F ON S.ID = F.ID
-LEFT JOIN PACKAGES P ON S.ID = P.ID
-LEFT JOIN PACKAGES PF ON F.Friend_ID = PF.ID
-where P.salary < PF.salary
-ORDER BY PF.salary
+WITH Start_Dates AS (
+    SELECT 
+    Start_Date,
+    ROW_NUMBER() OVER (ORDER BY Start_Date) AS RowNum
+    FROM Projects
+    WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)
+),
+End_Dates AS (
+    SELECT
+    End_Date,
+    ROW_NUMBER() OVER (ORDER BY End_Date) AS RowNum
+    FROM Projects
+    WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)
+)
+SELECT 
+    SD.Start_Date,
+    ED.End_Date
+FROM Start_Dates SD
+LEFT JOIN End_Dates ED ON SD.RowNum = ED.RowNum
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
