288f1a82e370792b2a5352c9a01393117274c658
mauricioavs <mauriciovelsa99@gmail.com>
2024-12-15 16:42:00 -0600

    added some problems

---
+++ HackerRank/Algorithms/Easy/Mini-Max_Sum/solution.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int32) {
+	// Write your code here
+	var minimum, maximum int32 = arr[0], arr[0]
+	var total int64 = 0
+	for _, num := range arr {
+		if num < minimum {
+			minimum = num
+		} else if num > maximum {
+			maximum = num
+		}
+		total += int64(num)
+	}
+	fmt.Printf("%d %d", total-int64(maximum), total-int64(minimum))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ HackerRank/Algorithms/Easy/Staircase/solution.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int32) {
+	// Write your code here
+	var i int32
+	for i = 1; i <= n; i++ {
+		spaces := strings.Repeat(" ", int(n-i))
+		hashtags := strings.Repeat("#", int(i))
+		fmt.Printf("%s%s\n", spaces, hashtags)
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
